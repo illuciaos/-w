8085836fdbefa28b1107375fc7bcdd32102daed1
illuciaos <123852800+illuciaos@users.noreply.github.com>
2023-01-29 11:14:11 +0700

    Create wq.lua

---
+++ wq.lua
@@ -0,0 +1,8 @@
+for i,v in pairs(game:GetService("Workspace").CandleWHITE:GetDescendants()) do
+    if v.Name == "ProximityPrompt" then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
+wait(.1)
+fireproximityprompt(v)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-15.3301163, 4.00000095, -108.875305, 0.998152137, -2.73452749e-09, 0.0607640408, 3.48092222e-09, 1, -1.21776402e-08, -0.0607640408, 1.23666526e-08, 0.998152137)
+end
+end
